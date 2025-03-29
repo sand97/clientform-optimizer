cbc5ae5bf5aa37145a2152f3c574c9b003ec6e9e
Bruce Guenkam <43252163+sand97@users.noreply.github.com>
2025-03-29 22:27:57 +0100

    Add Sentry

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,23 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import * as Sentry from "@sentry/react";
+import App from './App';
+import './index.css';
 
-createRoot(document.getElementById("root")!).render(<App />);
+Sentry.init({
+  dsn: "https://08bccdf81eda47353f0326f9abd7e93b@o1063428.ingest.us.sentry.io/4509063145652224",
+  // Enable performance monitoring
+  tracesSampleRate: 1.0,
+  // Enable session replay
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
+
+const container = document.getElementById('root') as HTMLElement;
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
